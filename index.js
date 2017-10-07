bf28fee95e064444bf25e14edb2afdb69002a32e
hweelin-yeo <yeohweelin_97@hotmail.com>
2017-10-07 13:53:26 -0400

    database

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var app = express();
+var pg = require('pg'); // database
+
 
 app.listen(process.env.PORT || 5000);
 
 app.get('/webhook', function(req, res) {
   res.send("test");
 })
+
 var Particle = require('particle-api-js');
 var particle = new Particle();
 var token;
+
 
 // Login
 particle.login({username: 'cornellresistance@gmail.com', password: 'clifford'}).then(
@@ -35,5 +39,17 @@ function getEventStream() {
     console.log(JSON.stringify(data, null, 4));
   });
 });
-
 }
+
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM rr_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
