30197587b6301031b5ddb623e98486e046dcd1ae
hweelin-yeo <yeohweelin_97@hotmail.com>
2017-10-08 00:46:24 -0400

    syntax

--- index.js
+++ index.js
@@ -67,22 +67,3 @@ app.post('/add', function (req, res) {
          // response.render('pages/db', {results: result.rows} ); }
     });
   });
-});
-
-
-function createPuppy(req, res, next) {
-  req.body.age = parseInt(req.body.age);
-  db.none('insert into pups(name, breed, age, sex)' +
-      'values(${name}, ${breed}, ${age}, ${sex})',
-    req.body)
-    .then(function () {
-      res.status(200)
-        .json({
-          status: 'success',
-          message: 'Inserted one puppy'
-        });
-    })
-    .catch(function (err) {
-      return next(err);
-    });
-}
