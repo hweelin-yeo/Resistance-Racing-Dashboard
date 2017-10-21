52e32a402fe3c0428ca87f6a41100f44453d9249
hweelin-yeo <yeohweelin_97@hotmail.com>
2017-10-21 17:14:28 -0400

    syntax error fix2

--- index.js
+++ index.js
@@ -80,6 +80,27 @@ app.post('/add', function (req, res) {
      };
     });
   });
+
+  function makeMockRequest() {
+    var request = require('request');
+
+
+    // Configure the request
+    var options = {
+      url: 'http://samwize.com',
+      method: 'GET',
+      headers: headers,
+      qs: {'key1': 'xxx', 'key2': 'yyy'}
+    }
+
+    // Start the request
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+          // Print out the response body
+          console.log(body)
+        }
+      })
+  }
 /**
   // Database: Post data
   app.post('/add', function (req, res) {
