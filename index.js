ebbc235846bb53862a750c6c206f83b46c5c4f60
hweelin-yeo <yeohweelin_97@hotmail.com>
2017-11-11 12:28:47 -0500

    fix webhook

--- index.js
+++ index.js
@@ -63,14 +63,14 @@ app.get('/db', function (req, res) {
 // Database: Post data
 app.post('/add', function (req, res) {
   console.log("reached add request function");
-  console.log(req.body.data.property);
+  console.log(req.body.data["property"]);
   console.log((JSON.parse(req.body.data)));
   console.log([
-    (JSON.parse(req.body.data)).property, (JSON.parse(req.body.data)).value]);
+    (JSON.parse(req.body.data))["property"], (JSON.parse(req.body.data))["value"]]);
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('INSERT INTO data (timestamp, property, value)' +
     'VALUES (NOW(), $1, $2)', [
-      (JSON.parse(req.body.data)).property, (JSON.parse(req.body.data)).value]); {
+      (JSON.parse(req.body.data))["property"], (JSON.parse(req.body.data)).["value"]]); {
       done();
       //res.redirect('/db');
 
