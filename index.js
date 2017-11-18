c7e31d9913489e38165e3b9db714bd5b08f2f8f7
hweelin-yeo <yeohweelin_97@hotmail.com>
2017-11-18 12:45:38 -0500

    json

--- index.js
+++ index.js
@@ -64,6 +64,8 @@ app.get('/db', function (req, res) {
 app.post('/add', function (req, res) {
   console.log("reached add request function");
   console.log("edit 2");
+  console.log(JSON.parse(req.body));
+  console.log(JSON.parse(JSON.parse(req.body)).data);
   console.log((JSON.parse(req.body.data)));
   console.log(JSON.parse(req.body['data']));
   console.log(req.body['data']);
